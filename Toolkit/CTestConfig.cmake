b9b5b327153c8b6249ed9ee745b3419b747eddcd
Luis Ibanez <luis.ibanez@kitware.com>
2008-08-22 11:22:19 -0400

    ENH: First Draft of infrastructure.
    SVN-Revision: 2

---
+++ Toolkit/CTestConfig.cmake
@@ -0,0 +1,6 @@
+SET(CTEST_PROJECT_NAME "ITK")
+SET(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
+SET(CTEST_DROP_METHOD "http")
+SET(CTEST_DROP_SITE "public.kitware.com")
+SET(CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+SET(CTEST_TRIGGER_SITE "http://public.kitware.com/cgi-bin/Submit-Insight-TestingResults.cgi")
