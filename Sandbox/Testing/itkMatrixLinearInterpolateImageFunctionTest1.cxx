cc512095dcadc0f0aca3c35d6aca3996b926ded6
Luis Ibanez <luis.ibanez@kitware.com>
2008-09-01 13:17:50 -0400

    ENH: Increasing code coverage.
    SVN-Revision: 37

--- Sandbox/Testing/itkMatrixLinearInterpolateImageFunctionTest1.cxx
+++ Sandbox/Testing/itkMatrixLinearInterpolateImageFunctionTest1.cxx
@@ -39,6 +39,95 @@ int main( int argc, char * argv [] )
   InterpolatorType2::Pointer interpolator2 = InterpolatorType2::New();
   InterpolatorType3::Pointer interpolator3 = InterpolatorType3::New();
 
+  ImageType1::RegionType region;
+  ImageType1::SizeType   size;
+  ImageType1::IndexType  start;
+
+  start.Fill( 0 );
+  size.Fill( 10 );
+
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  ImageType1::Pointer image1 = ImageType1::New();
+  image1->SetRegions( region );
+  image1->Allocate();
+
+  ImageType1::PixelType pixel1;
+
+  pixel1(0,0) =  7;
+  pixel1(0,1) =  9;
+  pixel1(1,0) = 11;
+  pixel1(1,1) = 13;
+
+  image1->FillBuffer( pixel1 );
+
+  interpolator1->SetInputImage( image1 );
+
+  InterpolatorType1::ContinuousIndexType cindex;
+
+  cindex[0] = 4.5;
+  cindex[1] = 4.5;
+
+  InterpolatorType1::OutputType value1 = interpolator1->EvaluateAtContinuousIndex( cindex );
+
+  for( unsigned row=0; row < NumberOfPhases; row++ )
+    {
+    for( unsigned col=0; col < Dimension; col++ )
+      {
+      if( value1(row,col) != pixel1(row,col) )
+        {
+        std::cerr << "Error in EvaluateAtContinuousIndex()" << std::endl;
+        return EXIT_FAILURE;
+        }
+      }
+    } 
+
+  InterpolatorType1::PointType point1;
+
+  point1[0] = 4.5;
+  point1[1] = 4.5;
+
+  value1 = interpolator1->Evaluate( point1 );
+
+  for( unsigned row=0; row < NumberOfPhases; row++ )
+    {
+    for( unsigned col=0; col < Dimension; col++ )
+      {
+      if( value1(row,col) != pixel1(row,col) )
+        {
+        std::cerr << "Error in EvaluateAtContinuousIndex()" << std::endl;
+        return EXIT_FAILURE;
+        }
+      }
+    } 
+
+  InterpolatorType1::IndexType index;
+
+  index[0] = 4;
+  index[1] = 4;
+
+  value1 = interpolator1->EvaluateAtIndex( index );
+
+  for( unsigned row=0; row < NumberOfPhases; row++ )
+    {
+    for( unsigned col=0; col < Dimension; col++ )
+      {
+      if( value1(row,col) != pixel1(row,col) )
+        {
+        std::cerr << "Error in EvaluateAtContinuousIndex()" << std::endl;
+        return EXIT_FAILURE;
+        }
+      }
+    } 
+
+
+  interpolator1->Print( std::cout );
+
+  std::cout << "Name of Class " << interpolator1->GetNameOfClass() << std::endl;
+  std::cout << "Name of SuperClass " << interpolator1->Superclass::GetNameOfClass() << std::endl;
+
+
   return EXIT_SUCCESS;
 }
 
