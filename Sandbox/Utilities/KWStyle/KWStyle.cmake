70d57191b2130a143ef23da129a1049c10227542
Karthik Krishnan <karthik.krishnan@kitware.com>
2008-10-19 18:16:19 -0400

    ENH: 1. Make the use of KWStyle an option by adding a boolean LesionSizingToolkit_USE_KWSTLE.      2. itkSystemInformation was added as an executable both from the         sandbox and the toolkit. This caused build errors when building         the toolkit and the sandbox from the top level. So, do not         build it from the sandbox.
    SVN-Revision: 101

--- Sandbox/Utilities/KWStyle/KWStyle.cmake
+++ Sandbox/Utilities/KWStyle/KWStyle.cmake
@@ -1,39 +1,42 @@
-OPTION(KWSTYLE_USE_VIM_FORMAT "Set KWStyle to generate errors with a VIM-compatible format." OFF)
-OPTION(KWSTYLE_USE_MSVC_FORMAT "Set KWStyle to generate errors with a VisualStudio-compatible format." OFF)
+OPTION(LesionSizingToolkit_USE_KWSTYLE "Enable the use of KWStyle for checking coding style." OFF)
+IF (LesionSizingToolkit_USE_KWSTYLE)
 
-FIND_PROGRAM(KWSTYLE_EXECUTABLE
-NAMES KWStyle 
-PATHS
-/usr/local/bin
-)
+  OPTION(KWSTYLE_USE_VIM_FORMAT "Set KWStyle to generate errors with a VIM-compatible format." OFF)
+  OPTION(KWSTYLE_USE_MSVC_FORMAT "Set KWStyle to generate errors with a VisualStudio-compatible format." OFF)
 
-CONFIGURE_FILE(
-  Utilities/KWStyle/ITKFiles.txt.in
-  ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITKFiles.txt)
-
-CONFIGURE_FILE(
-  Utilities/KWStyle/ITK.kws.xml.in
-  ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITK.kws.xml)
-
-SET(KWSTYLE_ARGUMENTS
-  -xml ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITK.kws.xml -v -D ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITKFiles.txt  
-  -o ${PROJECT_SOURCE_DIR}/Utilities/KWStyle/ITKOverwrite.txt
+  FIND_PROGRAM(KWSTYLE_EXECUTABLE
+  NAMES KWStyle 
+  PATHS
+  /usr/local/bin
   )
 
-IF(KWSTYLE_USE_VIM_FORMAT)
-  SET(KWSTYLE_ARGUMENTS -vim ${KWSTYLE_ARGUMENTS})
-ENDIF(KWSTYLE_USE_VIM_FORMAT)
+  CONFIGURE_FILE(
+    Utilities/KWStyle/ITKFiles.txt.in
+    ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITKFiles.txt)
 
-IF(KWSTYLE_USE_MSVC_FORMAT)
-  SET(KWSTYLE_ARGUMENTS -msvc ${KWSTYLE_ARGUMENTS})
-ENDIF(KWSTYLE_USE_MSVC_FORMAT)
+  CONFIGURE_FILE(
+    Utilities/KWStyle/ITK.kws.xml.in
+    ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITK.kws.xml)
 
-ADD_CUSTOM_COMMAND(
-  OUTPUT ${StatisticsRefactoring_BINARY_DIR}/KWStyleReport.txt
-  COMMAND ${KWSTYLE_EXECUTABLE}
-  ARGS    ${KWSTYLE_ARGUMENTS}
-  COMMENT "Coding Style Checker"
-  )
-ADD_CUSTOM_TARGET(StyleCheck DEPENDS ${StatisticsRefactoring_BINARY_DIR}/KWStyleReport.txt)
-ADD_TEST(KWStyleTest ${KWSTYLE_EXECUTABLE} ${KWSTYLE_ARGUMENTS})
+  SET(KWSTYLE_ARGUMENTS
+    -xml ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITK.kws.xml -v -D ${PROJECT_BINARY_DIR}/Utilities/KWStyle/ITKFiles.txt  
+    -o ${PROJECT_SOURCE_DIR}/Utilities/KWStyle/ITKOverwrite.txt
+    )
 
+  IF(KWSTYLE_USE_VIM_FORMAT)
+    SET(KWSTYLE_ARGUMENTS -vim ${KWSTYLE_ARGUMENTS})
+  ENDIF(KWSTYLE_USE_VIM_FORMAT)
+
+  IF(KWSTYLE_USE_MSVC_FORMAT)
+    SET(KWSTYLE_ARGUMENTS -msvc ${KWSTYLE_ARGUMENTS})
+  ENDIF(KWSTYLE_USE_MSVC_FORMAT)
+
+  ADD_CUSTOM_COMMAND(
+    OUTPUT ${StatisticsRefactoring_BINARY_DIR}/KWStyleReport.txt
+    COMMAND ${KWSTYLE_EXECUTABLE}
+    ARGS    ${KWSTYLE_ARGUMENTS}
+    COMMENT "Coding Style Checker"
+    )
+  ADD_CUSTOM_TARGET(StyleCheck DEPENDS ${StatisticsRefactoring_BINARY_DIR}/KWStyleReport.txt)
+  ADD_TEST(KWStyleTest ${KWSTYLE_EXECUTABLE} ${KWSTYLE_ARGUMENTS})
+ENDIF ( LesionSizingToolkit_USE_KWSTYLE )
