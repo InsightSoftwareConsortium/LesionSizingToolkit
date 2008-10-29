002e78bc1cd41af048d9a0afef90c12c25573edc
Luis Ibanez <luis.ibanez@kitware.com>
2008-10-29 12:47:50 -0400

    ENH: Improving test.
    SVN-Revision: 122

--- Sandbox/Testing/itkGradientMagnitudeSigmoidFeatureGeneratorTest1.cxx
+++ Sandbox/Testing/itkGradientMagnitudeSigmoidFeatureGeneratorTest1.cxx
@@ -18,19 +18,53 @@
 #include "itkSpatialObject.h"
 #include "itkImageSpatialObject.h"
 #include "itkImageMaskSpatialObject.h"
+#include "itkImageFileReader.h"
 
 int main( int argc, char * argv [] )
 {
+
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Arguments" << std::endl;
+    std::cerr << argv[0] << " inputImage outputImage " << std::endl;
+    return EXIT_FAILURE;
+    }
+
   const unsigned int Dimension = 3;
+
+  typedef signed short    InputPixelType;
+  typedef itk::Image< InputPixelType, Dimension >    InputImageType;
+
+  typedef itk::ImageFileReader< InputImageType >     ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( argv[1] );
+
+  try 
+    {
+    reader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
 
   typedef itk::GradientMagnitudeSigmoidFeatureGenerator< Dimension >   GradientMagnitudeSigmoidFeatureGeneratorType;
   typedef GradientMagnitudeSigmoidFeatureGeneratorType::SpatialObjectType    SpatialObjectType;
 
   GradientMagnitudeSigmoidFeatureGeneratorType::Pointer  featureGenerator = GradientMagnitudeSigmoidFeatureGeneratorType::New();
   
-  typedef itk::ImageSpatialObject< Dimension > ImageSpatialObjectType;
+  typedef itk::ImageSpatialObject< Dimension, signed short > ImageSpatialObjectType;
 
   ImageSpatialObjectType::Pointer inputObject = ImageSpatialObjectType::New();
+
+  InputImageType::Pointer inputImage = reader->GetOutput();
+
+  inputImage->DisconnectPipeline();
+
+  inputObject->SetImage( inputImage );
 
   featureGenerator->SetInput( inputObject );
 
