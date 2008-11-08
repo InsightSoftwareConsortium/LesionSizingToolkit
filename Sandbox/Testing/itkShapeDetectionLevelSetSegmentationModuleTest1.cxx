c5697f220ecbb9b252d06e82dc258d67f588ba40
Luis Ibanez <luis.ibanez@kitware.com>
2008-11-08 11:53:41 -0500

    ENH: Adding the maximusm number of iterations to the list of parameters in the command line.
    SVN-Revision: 189

--- Sandbox/Testing/itkShapeDetectionLevelSetSegmentationModuleTest1.cxx
+++ Sandbox/Testing/itkShapeDetectionLevelSetSegmentationModuleTest1.cxx
@@ -19,6 +19,7 @@
 #include "itkImageSpatialObject.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
 
 int main( int argc, char * argv [] )
 {
@@ -26,7 +27,8 @@ int main( int argc, char * argv [] )
   if( argc < 4 )
     {
     std::cerr << "Missing Arguments" << std::endl;
-    std::cerr << argv[0] << " inputImage featureImage outputImage [propagationScaling curvatureScaling]" << std::endl;
+    std::cerr << argv[0] << " inputImage featureImage outputImage ";
+    std::cerr << " [propagationScaling curvatureScaling maximumNumberOfIterations]" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -42,18 +44,35 @@ int main( int argc, char * argv [] )
   typedef SegmentationModuleType::OutputImageType      OutputImageType;
 
   typedef itk::ImageFileReader< InputImageType >       InputReaderType;
+  typedef itk::ImageFileWriter< InputImageType >       InputWriterType;
   typedef itk::ImageFileReader< FeatureImageType >     FeatureReaderType;
   typedef itk::ImageFileWriter< OutputImageType >      WriterType;
 
+  typedef itk::RescaleIntensityImageFilter< InputImageType, InputImageType >  RescaleFilterType;
+
   InputReaderType::Pointer inputReader = InputReaderType::New();
+  InputWriterType::Pointer inputWriter = InputWriterType::New();
   FeatureReaderType::Pointer featureReader = FeatureReaderType::New();
 
   inputReader->SetFileName( argv[1] );
+
+
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
+  rescaler->SetInput( inputReader->GetOutput() );
+
+  rescaler->SetOutputMinimum( -4.0 );
+  rescaler->SetOutputMaximum(  4.0 );
+
+  inputWriter->SetFileName( "inputSegmentation.mha" );
+
+  inputWriter->SetInput( rescaler->GetOutput() );
+  inputWriter->Update();
+
   featureReader->SetFileName( argv[2] );
 
   try 
     {
-    inputReader->Update();
+    rescaler->Update();
     featureReader->Update();
     }
   catch( itk::ExceptionObject & excp )
@@ -70,7 +89,7 @@ int main( int argc, char * argv [] )
   InputSpatialObjectType::Pointer inputObject = InputSpatialObjectType::New();
   FeatureSpatialObjectType::Pointer featureObject = FeatureSpatialObjectType::New();
 
-  inputObject->SetImage( inputReader->GetOutput() );
+  inputObject->SetImage( rescaler->GetOutput() );
   featureObject->SetImage( featureReader->GetOutput() );
 
   segmentationModule->SetInput( inputObject );
@@ -78,6 +97,7 @@ int main( int argc, char * argv [] )
 
   double propagationScaling = 1.0;
   double curvatureScaling = 1.0;
+  unsigned int maximumNumberOfIterations = 50;
 
   if( argc > 4 )
     {
@@ -89,8 +109,15 @@ int main( int argc, char * argv [] )
     curvatureScaling = atof( argv[5] ); 
     }
 
+  if( argc > 6 )
+    {
+    maximumNumberOfIterations = atoi( argv[6] ); 
+    }
+
+
   segmentationModule->SetPropagationScaling( propagationScaling );
   segmentationModule->SetCurvatureScaling( curvatureScaling );
+  segmentationModule->SetMaximumNumberOfIterations( maximumNumberOfIterations );
 
   segmentationModule->Update();
 
