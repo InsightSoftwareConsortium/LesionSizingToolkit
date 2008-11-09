230427034330105d8f0c12ebaa31aeb3af01b3ed
Luis Ibanez <luis.ibanez@kitware.com>
2008-11-08 23:31:00 -0500

    ENH: Adding the module containing the Confidence Connected method.
    SVN-Revision: 199

---
+++ Sandbox/Testing/itkConfidenceConnectedSegmentationModuleTest1.cxx
@@ -0,0 +1,160 @@
+/*=========================================================================
+
+  Program:   Lesion Sizing Toolkit
+  Module:    itkConfidenceConnectedSegmentationModuleTest1.cxx
+
+  Copyright (c) Kitware Inc. 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "itkConfidenceConnectedSegmentationModule.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageSpatialObject.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSpatialObjectReader.h"
+
+int main( int argc, char * argv [] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Arguments" << std::endl;
+    std::cerr << argv[0] << " landmarksFile featureImage outputImage ";
+    std::cerr << " [sigmaMultiplier] " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  const unsigned int Dimension = 3;
+
+  typedef itk::ConfidenceConnectedSegmentationModule< Dimension >   SegmentationModuleType;
+
+  typedef SegmentationModuleType::FeatureImageType     FeatureImageType;
+  typedef SegmentationModuleType::OutputImageType      OutputImageType;
+
+  typedef itk::ImageFileReader< FeatureImageType >     FeatureReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >      OutputWriterType;
+
+  typedef itk::SpatialObjectReader< 3, unsigned short > SpatialObjectReaderType;
+
+  SpatialObjectReaderType::Pointer landmarkPointsReader = SpatialObjectReaderType::New();
+
+  landmarkPointsReader->SetFileName( argv[1] );
+  landmarkPointsReader->Update();
+
+  SpatialObjectReaderType::ScenePointer scene = landmarkPointsReader->GetScene();
+
+  if( !scene )
+    {
+    std::cerr << "No Scene : [FAILED]" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Number of object in the scene:" << scene->GetNumberOfObjects(1) << std::endl;
+
+  typedef SpatialObjectReaderType::SceneType::ObjectListType     ObjectListType;
+
+  ObjectListType * sceneChildren = scene->GetObjects(999999);
+
+  ObjectListType::const_iterator spatialObjectItr = sceneChildren->begin();
+
+  typedef SegmentationModuleType::InputSpatialObjectType  InputSpatialObjectType; 
+
+  const InputSpatialObjectType * landmarkSpatialObject = NULL;
+
+  while( spatialObjectItr != sceneChildren->end() ) 
+    {
+    std::string objectName = (*spatialObjectItr)->GetTypeName();
+    if( objectName == "LandmarkSpatialObject" )
+      {
+      landmarkSpatialObject = 
+        dynamic_cast< const InputSpatialObjectType * >( spatialObjectItr->GetPointer() );
+      }
+    spatialObjectItr++;
+    }
+ 
+  FeatureReaderType::Pointer featureReader = FeatureReaderType::New();
+
+  featureReader->SetFileName( argv[2] );
+
+  try 
+    {
+    featureReader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  SegmentationModuleType::Pointer  segmentationModule = SegmentationModuleType::New();
+  
+  typedef SegmentationModuleType::InputSpatialObjectType          InputSpatialObjectType;
+  typedef SegmentationModuleType::FeatureSpatialObjectType        FeatureSpatialObjectType;
+  typedef SegmentationModuleType::OutputSpatialObjectType         OutputSpatialObjectType;
+
+  InputSpatialObjectType::Pointer inputObject = InputSpatialObjectType::New();
+  FeatureSpatialObjectType::Pointer featureObject = FeatureSpatialObjectType::New();
+
+  FeatureImageType::Pointer featureImage = featureReader->GetOutput();
+
+  featureImage->DisconnectPipeline();
+
+  featureImage->Print( std::cout );
+
+  featureObject->SetImage( featureImage );
+
+  segmentationModule->SetFeature( featureObject );
+  segmentationModule->SetInput( landmarkSpatialObject );
+
+  double sigmaMultiplier = 2.0;
+
+  if( argc > 4 )
+    {
+    sigmaMultiplier = atof( argv[4] );
+    }
+
+  segmentationModule->SetSigmaMultiplier( sigmaMultiplier );
+
+  segmentationModule->Update();
+
+  typedef SegmentationModuleType::SpatialObjectType    SpatialObjectType;
+  SpatialObjectType::ConstPointer segmentation = segmentationModule->GetOutput();
+
+  OutputSpatialObjectType::ConstPointer outputObject = 
+    dynamic_cast< const OutputSpatialObjectType * >( segmentation.GetPointer() );
+
+  OutputImageType::ConstPointer outputImage = outputObject->GetImage();
+
+  OutputWriterType::Pointer writer = OutputWriterType::New();
+
+  writer->SetFileName( argv[3] );
+  writer->SetInput( outputImage );
+
+
+  try 
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  segmentationModule->Print( std::cout );
+
+  std::cout << "Class name = " << segmentationModule->GetNameOfClass() << std::endl;
+  
+  return EXIT_SUCCESS;
+}
