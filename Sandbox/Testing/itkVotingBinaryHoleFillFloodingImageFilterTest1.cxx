869a1f1888f1984dc7f3cabe57283057681947b3
Luis Ibanez <luis.ibanez@kitware.com>
2008-12-17 18:09:13 -0500

    ENH: Adding specific test for the itkVotingBinaryHoleFillFloodingImageFilter in order to increase code coverage.
    SVN-Revision: 303

---
+++ Sandbox/Testing/itkVotingBinaryHoleFillFloodingImageFilterTest1.cxx
@@ -0,0 +1,107 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVotingBinaryHoleFillFloodingImageFilterTest1.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkVotingBinaryHoleFillFloodingImageFilter.h"
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 6 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImageFile outputImageFile radius majority maxNumberOfIterations" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  typedef   unsigned char  PixelType;
+
+  const unsigned int Dimension = 3;
+
+  typedef itk::Image< PixelType, Dimension >   ImageType;
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  typedef itk::ImageFileWriter< ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+  typedef itk::BinaryThresholdImageFilter< ImageType, ImageType > ThresholderType;
+
+  ThresholderType::Pointer thresholder = ThresholderType::New();
+
+  thresholder->SetLowerThreshold( atoi( argv[3] ) );
+  thresholder->SetUpperThreshold( itk::NumericTraits< PixelType >::max() );
+
+  thresholder->SetOutsideValue(  0 );
+  thresholder->SetInsideValue( 255 );
+ 
+  typedef itk::VotingBinaryHoleFillFloodingImageFilter< ImageType, ImageType >  FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  const unsigned int radius = atoi( argv[3] );
+
+  ImageType::SizeType indexRadius;
+  
+  indexRadius[0] = radius; // radius along x
+  indexRadius[1] = radius; // radius along y
+  indexRadius[1] = radius; // radius along y
+
+  filter->SetRadius( indexRadius );
+
+  filter->SetBackgroundValue(   0 );
+  filter->SetForegroundValue( 255 );
+
+  const unsigned int majorityThreshold = atoi( argv[4] );
+
+  filter->SetMajorityThreshold( majorityThreshold  );
+
+  const unsigned int maximumNumberOfIterations = atoi( argv[5] );
+
+  filter->SetMaximumNumberOfIterations( maximumNumberOfIterations  );
+
+  if( filter->GetMaximumNumberOfIterations() != maximumNumberOfIterations )
+    {
+    std::cerr << "Error in Set/GetMaximumNumberOfIterations() " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+
+  std::cout << "Class name " << filter->GetNameOfClass() << std::endl;
+  
+  filter->Print( std::cout );
+
+  std::cout << "Iteration used = " << filter->GetCurrentIterationNumber()     << std::endl;
+  std::cout << "Pixels changes = " << filter->GetTotalNumberOfPixelsChanged() << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
