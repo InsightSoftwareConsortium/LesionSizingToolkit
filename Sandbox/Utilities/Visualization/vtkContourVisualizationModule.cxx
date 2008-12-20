7d70546a7a62a14d83282780ab63d46d0266370b
Luis Ibanez <luis.ibanez@kitware.com>
2008-12-20 09:46:44 -0500

    ENH: Adding helper class that will generate the contour from a segmentation.
    SVN-Revision: 343

---
+++ Sandbox/Utilities/Visualization/vtkContourVisualizationModule.cxx
@@ -0,0 +1,277 @@
+/*=========================================================================
+
+  Program:   Image Analysis Platform 
+  Module:    vtkContourVisualizationModule.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Kitware Inc. All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "vtkContourVisualizationModule.h"
+#include "vtkObjectFactory.h"
+#include "vtkContourFilter.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkProperty.h"
+#include "vtkRenderer.h"
+#include "vtkImageData.h"
+#include "vtkImageReslice.h"
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro( vtkContourVisualizationModule );
+vtkCxxRevisionMacro(vtkContourVisualizationModule, "1.2");
+
+
+vtkContourVisualizationModule::vtkContourVisualizationModule()
+{
+  this->ContourFilter = vtkContourFilter::New();
+  this->PolyDataMapper = vtkPolyDataMapper::New();
+  this->Actor = vtkActor::New();
+  this->Property = vtkProperty::New();
+  this->ResliceFilter = vtkImageReslice::New();
+
+  this->ResliceFilter->SetOutputDimensionality(3);
+  this->ResliceFilter->SetInterpolationModeToLinear();
+
+  this->ContourFilter->SetInput( this->ResliceFilter->GetOutput() );
+
+  this->PolyDataMapper->SetInput( this->ContourFilter->GetOutput() );
+  this->PolyDataMapper->ScalarVisibilityOff();
+  this->PolyDataMapper->SetResolveCoincidentTopologyToPolygonOffset();
+
+  this->Actor->SetMapper( this->PolyDataMapper );
+
+  this->Property->SetRepresentationToSurface();
+  this->Property->SetAmbient(0.1);
+  this->Property->SetDiffuse(0.1);
+  this->Property->SetSpecular(0.5);
+  this->Property->SetLineWidth(2.0);
+  this->Property->SetColor(1.0,0.0,0.0);
+  this->Actor->SetProperty( this->Property );
+
+  
+  this->Color[0] = 1.0;
+  this->Color[1] = 0.0;
+  this->Color[2] = 0.0;
+
+  this->Visibility = 1;
+
+  this->SlicePosition[0] = 0;
+  this->SlicePosition[1] = 0;
+  this->SlicePosition[2] = 0;
+
+  this->SliceOrientation = 0;
+}
+
+vtkContourVisualizationModule::~vtkContourVisualizationModule()
+{
+  if( this->ContourFilter )
+    {
+    this->ContourFilter->Delete();
+    }
+  if( this->Actor )
+    {
+    this->Actor->Delete();
+    }
+  if( this->PolyDataMapper )
+    {
+    this->PolyDataMapper->Delete();
+    }
+  if( this->Property )
+    {
+    this->Property->Delete();
+    }
+  if( this->ResliceFilter )
+    {
+    this->ResliceFilter->Delete();
+    }
+}
+
+
+void vtkContourVisualizationModule::SetSegmentation( vtkImageData * input )
+{
+  // Draw contours around the segmented regions
+  this->ResliceFilter->SetInput( input );
+  double range[2];
+  this->GetScalarRange(range);
+  this->ContourFilter->SetValue( 0, (range[1] - range[0]) / 2.0 ); 
+}
+
+double vtkContourVisualizationModule::GetIsoValue()
+{
+  return this->ContourFilter->GetValue(0);
+}
+
+void vtkContourVisualizationModule::SetContourColor( double r, double g, double b)
+{
+  this->Color[0] = r;
+  this->Color[1] = g;
+  this->Color[2] = b;
+  
+  this->Property->SetColor( r , g , b ); 
+}
+
+void vtkContourVisualizationModule::GetContourColor( double &r, double &g , double &b)
+{
+  r = this->Color[0]; 
+  g = this->Color[1];
+  b = this->Color[2]; 
+}
+
+int vtkContourVisualizationModule::GetVisibility() 
+{
+  return this->Visibility;
+}
+
+void vtkContourVisualizationModule::SetContourVisibility( int state  )
+{
+  this->Visibility = state;
+  this->Actor->SetVisibility( state ); 
+}
+
+vtkActor * 
+vtkContourVisualizationModule::GetActor()
+{
+  return this->Actor;
+}
+
+void vtkContourVisualizationModule::SetPlaneOrigin( double origin[3] )
+{
+  this->SlicePosition[0] = origin[0];
+  this->SlicePosition[1] = origin[1];
+  this->SlicePosition[2] = origin[2];
+
+  vtkImageData * segmentation = 
+    dynamic_cast<vtkImageData *>( this->ResliceFilter->GetInput() );
+
+  double * segmentationOrigin  = segmentation->GetOrigin();
+
+  double sliceOrigin[3];
+
+  sliceOrigin[0] = segmentationOrigin[0];
+  sliceOrigin[1] = segmentationOrigin[1];
+  sliceOrigin[2] = segmentationOrigin[2];
+
+  switch( this->SliceOrientation )
+    {
+    case SLICE_ORIENTATION_YZ:
+      {
+      sliceOrigin[0] = this->SlicePosition[0];
+      break;
+      }
+    case SLICE_ORIENTATION_XZ:
+      {
+      sliceOrigin[1] = this->SlicePosition[1];
+      break;
+      }
+    case SLICE_ORIENTATION_XY:
+      {
+      sliceOrigin[2] = this->SlicePosition[2];
+      break;
+      }
+    }
+
+  this->ResliceFilter->SetOutputOrigin( sliceOrigin );
+  this->ContourFilter->Update();
+}
+
+void vtkContourVisualizationModule::SetPlaneOrientation( int orientation )
+{
+  this->SliceOrientation = orientation;
+
+  vtkImageData * segmentation = 
+    dynamic_cast<vtkImageData *>( this->ResliceFilter->GetInput() );
+
+  double * segmentationOrigin  = segmentation->GetOrigin();
+  int    * segmentationExtent  = segmentation->GetExtent();
+
+  double sliceOrigin[3];
+  int    sliceExtent[6];
+
+  sliceExtent[0] = segmentationExtent[0]; 
+  sliceExtent[1] = segmentationExtent[1]; 
+  sliceExtent[2] = segmentationExtent[2]; 
+  sliceExtent[3] = segmentationExtent[3]; 
+  sliceExtent[4] = segmentationExtent[4]; 
+  sliceExtent[5] = segmentationExtent[5];
+
+  sliceOrigin[0] = segmentationOrigin[0];
+  sliceOrigin[1] = segmentationOrigin[1];
+  sliceOrigin[2] = segmentationOrigin[2];
+
+  switch( orientation )
+    {
+    case SLICE_ORIENTATION_YZ:
+      {
+      sliceExtent[0] = 0;
+      sliceExtent[1] = 0;
+      sliceOrigin[0] = this->SlicePosition[0];
+      break;
+      }
+    case SLICE_ORIENTATION_XZ:
+      {
+      sliceExtent[2] = 0;
+      sliceExtent[3] = 0;
+      sliceOrigin[1] = this->SlicePosition[1];
+      break;
+      }
+    case SLICE_ORIENTATION_XY:
+      {
+      sliceExtent[4] = 0;
+      sliceExtent[5] = 0;
+      sliceOrigin[2] = this->SlicePosition[2];
+      break;
+      }
+    }
+
+  this->ResliceFilter->SetOutputOrigin( sliceOrigin );
+
+  this->ResliceFilter->SetOutputSpacing( segmentation->GetSpacing() );
+
+  this->ResliceFilter->SetOutputExtent( 
+           sliceExtent[0], sliceExtent[1],
+           sliceExtent[2], sliceExtent[3],
+           sliceExtent[4], sliceExtent[5] );
+
+  this->ContourFilter->Update();
+}
+
+void vtkContourVisualizationModule::ForceContourUpdate()
+{
+  this->ResliceFilter->Modified();
+  this->ContourFilter->Update();
+}
+
+void vtkContourVisualizationModule::SetIsoValue( double isovalue )
+{
+  this->ContourFilter->SetValue( 0, isovalue );
+}
+
+void vtkContourVisualizationModule::GetScalarRange(double range[2])
+{
+  range[0] = VTK_DOUBLE_MAX;
+  range[1] = VTK_DOUBLE_MIN;
+
+  vtkImageData * segmentation = 
+    dynamic_cast<vtkImageData *>( this->ResliceFilter->GetInput() );
+  if (segmentation)
+    {
+    segmentation->GetScalarRange( range );
+    // deal with case of new segmentation
+    if( range[0] == range[1] &&
+        segmentation->GetScalarType() == VTK_UNSIGNED_CHAR)
+      {
+      range[0] = 0;
+      range[1] = 255;
+      }
+    }
+}
+
---
+++ Sandbox/Utilities/Visualization/vtkContourVisualizationModule.h
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Image Analysis Platform 
+  Module:    vtkContourVisualizationModule.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Kitware Inc. All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __vtkContourVisualizationModule_h
+#define __vtkContourVisualizationModule_h
+
+#include "vtkObject.h"
+#include "itkVector.h"
+
+class vtkContourFilter;
+class vtkPolyDataMapper;
+class vtkActor;
+class vtkProperty;
+class vtkImageData;
+class vtkImageReslice;
+
+
+class vtkContourVisualizationModule : public vtkObject
+{
+public:
+  static vtkContourVisualizationModule* New();
+  vtkTypeRevisionMacro(vtkContourVisualizationModule,vtkObject);
+
+  // Image that holds the segmentation represented as a grayscale map This
+  // representation is similar to a level set. The object is defined by the
+  // iso-contour at level 127.5
+  void SetSegmentation( vtkImageData * );
+
+  // Origin and Orientation of the plane for which the contour is going to be
+  // visualized.  This class only does visualization in orthogonal planes.
+  // This class uses the Cutter filter in order to extract one slice from the
+  // image, then applies the Contour filter to that slice. The resulting
+  // contour as a vtkPolyData structure is passed to an Actor for
+  // visualization.
+  void SetPlaneOrigin( double origin[3] );
+  void SetPlaneOrientation( int orientation );
+
+  // Return the actor that is capable of displaying the extracted contour.
+  // This actor can now be added to a vtkRenderer in order to compose a
+  // visualization of the image and the overlapping contour of the
+  // segmentation.
+  vtkActor * GetActor();
+
+  // set the color of the contour
+  void SetContourColor( double r, double g, double b );  
+
+  // Get contour color
+  void GetContourColor( double &r, double &g, double &b );  
+
+  // Set the visibility of the contour
+  void SetContourVisibility( int state );  
+
+  // Get the visibility of the contour
+  int GetVisibility(); 
+
+  // Get the scalar range of the input image. Returns 
+  // (VTK_DOUBLE_MAX, VTK_DOUBLE_MIN) if no data exists.
+  void GetScalarRange(double range[2]);
+
+  // Get the iso-value
+  double GetIsoValue();
+
+  void SetIsoValue(double isovalue);
+  
+  // Update the contour filter to reflect the current state
+  // of the segmentation.
+  void ForceContourUpdate();
+
+  // Slice orientations used by ImageViewer
+  //BTX
+  enum
+    {
+    SLICE_ORIENTATION_YZ = 0,
+    SLICE_ORIENTATION_XZ = 1,
+    SLICE_ORIENTATION_XY = 2
+    };
+  //ETX
+
+protected:
+  vtkContourVisualizationModule();
+  ~vtkContourVisualizationModule();
+
+private:
+  vtkContourVisualizationModule(const vtkContourVisualizationModule&);   // Not implemented.
+  void operator=(const vtkContourVisualizationModule&);  // Not implemented.
+
+  // Objects need for displaying contours
+  vtkContourFilter               *ContourFilter;
+  vtkPolyDataMapper              *PolyDataMapper;
+  vtkActor                       *Actor;
+  vtkProperty                    *Property;
+  vtkImageReslice                *ResliceFilter;
+
+  // properites that will be used by the visualization interface
+  int                             Visibility;
+  double                          Color[3];
+
+  // Position of the currently selected point in 3D
+  // The slices will be cut across this point.
+  double                          SlicePosition[3];
+
+  // Orientation of the current slice across the dataset
+  int                             SliceOrientation;
+};
+
+#endif
