f61a4007436f7a4a99a669eac68434fa51527916
Luis Ibanez <luis.ibanez@kitware.com>
2008-12-20 10:29:49 -0500

    ENH: Adding reading of landmarks in order to have a point of reference through which cut the plane of the slice.
    SVN-Revision: 346

--- Sandbox/Utilities/Visualization/ViewImageSlicesAndSegmentationContours.cxx
+++ Sandbox/Utilities/Visualization/ViewImageSlicesAndSegmentationContours.cxx
@@ -25,6 +25,10 @@
 #include "vtkRenderWindowInteractor.h"
 #include "vtkInteractorStyleImage.h"
 
+#include "itkSpatialObject.h"
+#include "itkSpatialObjectReader.h"
+#include "itkLandmarkSpatialObject.h"
+
 #define VTK_CREATE(type, name) \
   vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
 
@@ -34,15 +38,71 @@ int main(int argc, char * argv [] )
   // Load a scalar image and a segmentation mask and display the
   // scalar image with the contours of the mask overlaid on it.
 
-  if( argc < 3 )
+  if( argc < 4 )
     {
     std::cerr << "Missing parameters" << std::endl;
-    std::cerr << "Usage: " << argv[0] << " imageFileName segmentationFilename";
+    std::cerr << "Usage: " << argv[0] << " imageFileName landmarkFile segmentationFilename";
     std::cerr << "[segmentationFilename2,...n]";
     std::cerr << std::endl;
     return 1;
     }
   
+
+  typedef itk::SpatialObjectReader< 3, unsigned short > SpatialObjectReaderType;
+
+  SpatialObjectReaderType::Pointer landmarkPointsReader = SpatialObjectReaderType::New();
+
+  landmarkPointsReader->SetFileName( argv[1] );
+  landmarkPointsReader->Update();
+
+  SpatialObjectReaderType::ScenePointer scene = landmarkPointsReader->GetScene();
+
+  if( !scene )
+    {
+    std::cerr << "No Scene : [FAILED]" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Number of object in the scene:" << scene->GetNumberOfObjects(1) << std::endl;
+
+  typedef SpatialObjectReaderType::SceneType::ObjectListType     ObjectListType;
+
+  ObjectListType * sceneChildren = scene->GetObjects(999999);
+
+  ObjectListType::const_iterator spatialObjectItr = sceneChildren->begin();
+
+  /** Type of the input set of seed points. They are stored in a Landmark Spatial Object. */
+  typedef itk::LandmarkSpatialObject< 3 >  InputSpatialObjectType;
+
+  const InputSpatialObjectType * landmarkSpatialObject = NULL;
+
+  while( spatialObjectItr != sceneChildren->end() ) 
+    {
+    std::string objectName = (*spatialObjectItr)->GetTypeName();
+    if( objectName == "LandmarkSpatialObject" )
+      {
+      landmarkSpatialObject = 
+        dynamic_cast< const InputSpatialObjectType * >( spatialObjectItr->GetPointer() );
+      }
+    spatialObjectItr++;
+    }
+ 
+  typedef InputSpatialObjectType::PointListType            PointListType;
+  typedef InputSpatialObjectType::SpatialObjectPointType   SpatialObjectPointType;
+  typedef SpatialObjectPointType::PointType                PointType;
+
+
+  const PointListType & points = landmarkSpatialObject->GetPoints();
+
+  // Grab the first point in the list of seed points
+  PointType point = points[0].GetPosition();
+
+  double seedPoint[3];
+
+  seedPoint[0] = point[0];
+  seedPoint[1] = point[1];
+  seedPoint[2] = point[2];
+
   typedef vtkSmartPointer< vtkContourVisualizationModule >  vtkContourVisualizationModulePointer;
   typedef std::vector< vtkContourVisualizationModulePointer >    ContoursContainer;
 
@@ -75,16 +135,22 @@ int main(int argc, char * argv [] )
 
   renderer->AddActor( imageViewer->GetImageActor() );
 
-  const unsigned int numberOfSegmentations = argc - 2;
+  const unsigned int numberOfArgumentsBeforeSegmentations = 3;
+  const unsigned int numberOfSegmentations = argc - numberOfArgumentsBeforeSegmentations;
 
   for(unsigned int segmentationId=0; segmentationId < numberOfSegmentations; segmentationId++)
     {
     VTK_CREATE( vtkMetaImageReader, segmentationReader );
-    segmentationReader->SetFileName( argv[segmentationId+2] );
+    segmentationReader->SetFileName( argv[segmentationId+numberOfArgumentsBeforeSegmentations] );
     segmentationReader->Update();
 
     VTK_CREATE( vtkContourVisualizationModule, newContourModule );
     contourModules.push_back( newContourModule );
+
+    newContourModule->SetIsoValue( 0.0 );
+    newContourModule->SetContourColor( 1, 0, 0 );
+    newContourModule->SetContourVisibility( 1 );
+    newContourModule->SetPlaneOrigin( seedPoint );
 
     newContourModule->SetSegmentation( segmentationReader->GetOutput() );
     renderer->AddActor( newContourModule->GetActor() );
