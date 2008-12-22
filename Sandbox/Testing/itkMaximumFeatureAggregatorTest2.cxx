adc16588f27654a482b11721cf26dadce4d46319
Luis Ibanez <luis.ibanez@kitware.com>
2008-12-22 18:24:05 -0500

    ENH: Adding a Maximum feature aggregator.
    SVN-Revision: 398

---
+++ Sandbox/Testing/itkMaximumFeatureAggregatorTest2.cxx
@@ -0,0 +1,222 @@
+/*=========================================================================
+
+  Program:   Lesion Sizing Toolkit
+  Module:    itkMaximumFeatureAggregatorTest2.cxx
+
+  Copyright (c) Kitware Inc. 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "itkMaximumFeatureAggregator.h"
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageMaskSpatialObject.h"
+#include "itkLungWallFeatureGenerator.h"
+#include "itkSatoVesselnessSigmoidFeatureGenerator.h"
+#include "itkSigmoidFeatureGenerator.h"
+#include "itkConnectedThresholdSegmentationModule.h"
+
+namespace itk
+{
+
+class MaximumFeatureAggregatorDerived : public MaximumFeatureAggregator<3>
+{
+public:
+  /** Standard class typedefs. */
+  typedef MaximumFeatureAggregatorDerived     Self;
+  typedef MaximumFeatureAggregator<3>         Superclass;
+  typedef SmartPointer<Self>                  Pointer;
+  typedef SmartPointer<const Self>            ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(MaximumFeatureAggregatorDerived, MaximumFeatureAggregator);
+
+  typedef  Superclass::InputFeatureType    InputFeatureType;
+
+  //
+  // Need to make it public, to make testing possible.
+  //
+  const InputFeatureType * GetInputFeature( unsigned int id ) const
+    {
+    return this->Superclass::GetInputFeature( id );
+    }
+};
+
+}
+
+int main( int argc, char * argv [] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Arguments" << std::endl;
+    std::cerr << argv[0] << " landmarksFile inputImage outputImage ";
+    std::cerr << " [lowerThreshold upperThreshold] " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  const unsigned int Dimension = 3;
+  typedef signed short   InputPixelType;
+
+  typedef itk::Image< InputPixelType, Dimension > InputImageType;
+
+  typedef itk::ImageFileReader< InputImageType > InputImageReaderType;
+  InputImageReaderType::Pointer inputImageReader = InputImageReaderType::New();
+
+  inputImageReader->SetFileName( argv[2] );
+
+  try 
+    {
+    inputImageReader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  typedef itk::MaximumFeatureAggregatorDerived  AggregatorType;
+
+  AggregatorType::Pointer  featureAggregator = AggregatorType::New();
+  
+  typedef itk::SatoVesselnessSigmoidFeatureGenerator< Dimension > VesselnessGeneratorType;
+  VesselnessGeneratorType::Pointer vesselnessGenerator = VesselnessGeneratorType::New();
+
+  typedef itk::LungWallFeatureGenerator< Dimension > LungWallGeneratorType;
+  LungWallGeneratorType::Pointer lungWallGenerator = LungWallGeneratorType::New();
+
+  typedef itk::SigmoidFeatureGenerator< Dimension >   SigmoidFeatureGeneratorType;
+  SigmoidFeatureGeneratorType::Pointer  sigmoidGenerator = SigmoidFeatureGeneratorType::New();
+ 
+  featureAggregator->AddFeatureGenerator( lungWallGenerator );
+  featureAggregator->AddFeatureGenerator( vesselnessGenerator );
+  featureAggregator->AddFeatureGenerator( sigmoidGenerator );
+
+
+  typedef AggregatorType::SpatialObjectType    SpatialObjectType;
+
+  typedef itk::ImageSpatialObject< Dimension, InputPixelType  > InputImageSpatialObjectType;
+  InputImageSpatialObjectType::Pointer inputObject = InputImageSpatialObjectType::New();
+
+  InputImageType::Pointer inputImage = inputImageReader->GetOutput();
+
+  inputImage->DisconnectPipeline();
+
+  inputObject->SetImage( inputImage );
+
+  lungWallGenerator->SetInput( inputObject );
+  vesselnessGenerator->SetInput( inputObject );
+  sigmoidGenerator->SetInput( inputObject );
+
+  lungWallGenerator->SetLungThreshold( -400 );
+
+  vesselnessGenerator->SetSigma( 1.0 );
+  vesselnessGenerator->SetAlpha1( 0.5 );
+  vesselnessGenerator->SetAlpha2( 2.0 );
+ 
+  sigmoidGenerator->SetAlpha(  1.0  );
+  sigmoidGenerator->SetBeta( -200.0 );
+ 
+  typedef itk::ConnectedThresholdSegmentationModule< Dimension >   SegmentationModuleType;
+  
+  SegmentationModuleType::Pointer  segmentationModule = SegmentationModuleType::New();
+
+
+  double lowerThreshold = 0.5;
+  double upperThreshold = 1.0;
+
+  if( argc > 4 )
+    {
+    lowerThreshold = atof( argv[4] );
+    }
+
+  if( argc > 5 )
+    {
+    upperThreshold = atof( argv[5] );
+    }
+
+  segmentationModule->SetLowerThreshold( lowerThreshold );
+  segmentationModule->SetUpperThreshold( upperThreshold );
+
+
+  featureAggregator->Update();
+
+  
+  typedef SegmentationModuleType::SpatialObjectType           SpatialObjectType;
+  typedef SegmentationModuleType::OutputSpatialObjectType     OutputSpatialObjectType;
+  typedef SegmentationModuleType::OutputImageType             OutputImageType;
+
+  SpatialObjectType::ConstPointer segmentation = featureAggregator->GetFeature();
+
+  OutputSpatialObjectType::ConstPointer outputObject = 
+    dynamic_cast< const OutputSpatialObjectType * >( segmentation.GetPointer() );
+
+  OutputImageType::ConstPointer outputImage = outputObject->GetImage();
+
+  typedef itk::ImageFileWriter< OutputImageType >      OutputWriterType;
+  OutputWriterType::Pointer writer = OutputWriterType::New();
+
+  writer->SetFileName( argv[3] );
+  writer->SetInput( outputImage );
+  writer->UseCompressionOn();
+
+
+  try 
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  featureAggregator->Print( std::cout );
+
+  //
+  // Exercise GetInputFeature()
+  //
+  if( featureAggregator->GetInputFeature( 0 ) != lungWallGenerator->GetFeature() )
+    {
+    std::cerr << "Failure to recover feature 0 with GetInputFeature()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  if( featureAggregator->GetInputFeature( 1 ) != vesselnessGenerator->GetFeature() )
+    {
+    std::cerr << "Failure to recover feature 1 with GetInputFeature()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  if( featureAggregator->GetInputFeature( 2 ) != sigmoidGenerator->GetFeature() )
+    {
+    std::cerr << "Failure to recover feature 2 with GetInputFeature()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  try 
+    {
+    featureAggregator->GetInputFeature( 3 );
+    std::cerr << "Failure to catch an exception for GetInputFeature() with out of range argument" << std::endl;
+    return EXIT_FAILURE;
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cout << "Caught expected exception" << std::endl;
+    std::cout << excp << std::endl;
+    }
+
+  return EXIT_SUCCESS;
+}
