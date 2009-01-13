eadd22e0bd720e5d2bd06ce580587bb1ef533d0e
Luis Ibanez <luis.ibanez@kitware.com>
2009-01-13 14:53:35 -0500

    ENH: Adding helper command line tests for computing the Laplacian and GradientMagnitude using RecursiveGaussian filters.
    SVN-Revision: 441

---
+++ Sandbox/Testing/GradientMagnitudeRecursiveGaussianImageFilter.cxx
@@ -0,0 +1,87 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    GradientMagnitudeRecursiveGaussianImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 4 ) 
+    { 
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile   outputImageFile   sigma" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  typedef    float    InputPixelType;
+  typedef    float    OutputPixelType;
+
+  const unsigned Dimension = 2;
+
+  typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >   OutputImageType;
+
+
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+
+
+  typedef itk::GradientMagnitudeRecursiveGaussianImageFilter<
+                        InputImageType, OutputImageType >  FilterType;
+
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  FilterType::Pointer filter = FilterType::New();
+
+  filter->SetInput( reader->GetOutput() );
+
+  const double sigma = atof( argv[3] );
+
+  filter->SetSigma( sigma );
+
+  filter->Update();
+
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName( argv[2] );
+ 
+  writer->SetInput( filter->GetOutput() );
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionMacro & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
---
+++ Sandbox/Testing/LaplacianRecursiveGaussianImageFilter.cxx
@@ -0,0 +1,83 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    LaplacianRecursiveGaussianImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkLaplacianRecursiveGaussianImageFilter.h"
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 4 ) 
+    { 
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile  outputImageFile  sigma " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef    float    InputPixelType;
+  typedef    float    OutputPixelType;
+
+  const unsigned int Dimension = 3;
+
+  typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >   OutputImageType;
+
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+
+  typedef itk::LaplacianRecursiveGaussianImageFilter<
+                        InputImageType, OutputImageType >  FilterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  FilterType::Pointer laplacian = FilterType::New();
+
+  laplacian->SetNormalizeAcrossScale( false );
+
+  laplacian->SetInput( reader->GetOutput() );
+
+  const double sigma = atof( argv[3] );
+
+  laplacian->SetSigma( sigma );
+
+  try
+    {
+    laplacian->Update();
+    }
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetInput( laplacian->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
+
