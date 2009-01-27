dc82438a2f0c10c919ea46e014a33577bfd78c65
Luis Ibanez <luis.ibanez@kitware.com>
2009-01-26 21:56:10 -0500

    ENH: Adding process object for computing the resampling of an input image to an isotropic spacing.
    SVN-Revision: 545

---
+++ Sandbox/Testing/itkIsotropicResamplerTest1.cxx
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program:   Lesion Sizing Toolkit
+  Module:    itkIsotropicResamplerTest1.cxx
+
+  Copyright (c) Kitware Inc. 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "itkIsotropicResampler.h"
+#include "itkImage.h"
+#include "itkSpatialObject.h"
+#include "itkImageSpatialObject.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+int main( int argc, char * argv [] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Arguments" << std::endl;
+    std::cerr << argv[0] << " inputImage outputImage " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const unsigned int Dimension = 3;
+
+  typedef signed short    InputPixelType;
+  typedef signed short    OutputPixelType;
+
+  typedef itk::Image< InputPixelType,  Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >   OutputImageType;
+
+  typedef itk::ImageFileReader< InputImageType >     ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >    WriterType;
+
+  typedef itk::ImageSpatialObject< Dimension, InputPixelType  > InputImageSpatialObjectType;
+  typedef itk::ImageSpatialObject< Dimension, OutputPixelType > OutputImageSpatialObjectType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( argv[1] );
+
+  try 
+    {
+    reader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::IsotropicResampler< Dimension >   ResampleFilterType;
+  typedef ResampleFilterType::SpatialObjectType    SpatialObjectType;
+
+  ResampleFilterType::Pointer  resampler = ResampleFilterType::New();
+  
+
+  InputImageSpatialObjectType::Pointer inputObject = InputImageSpatialObjectType::New();
+
+  InputImageType::Pointer inputImage = reader->GetOutput();
+
+  inputImage->DisconnectPipeline();
+
+  inputObject->SetImage( inputImage );
+
+  resampler->SetInput( inputObject );
+
+  try 
+    {
+    resampler->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  SpatialObjectType::ConstPointer resampledImage = resampler->GetOutput();
+
+  OutputImageSpatialObjectType::ConstPointer outputObject = 
+    dynamic_cast< const OutputImageSpatialObjectType * >( resampledImage.GetPointer() );
+
+  OutputImageType::ConstPointer outputImage = outputObject->GetImage();
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName( argv[2] );
+  writer->SetInput( outputImage );
+  writer->UseCompressionOn();
+
+
+  try 
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Name Of Class = " << resampler->GetNameOfClass() << std::endl;
+
+  resampler->Print( std::cout );
+ 
+  return EXIT_SUCCESS;
+}
