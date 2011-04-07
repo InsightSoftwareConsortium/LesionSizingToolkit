09ead1f80dd6c92e362138ae8051a4f063ba5470
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2011-04-07 16:28:21 -0400

    ENH: Remove unused Toolkit/ directory and top-level CMakeLists.txt
    The toolkit lies entirely in the Sandbox directory.  Remove the old
    placeholder source tree.  Also remove old subversion support scripts.

--- svnadd.sh
+++
@@ -1,4 +0,0 @@
-svn add $1
-svn propset svn:mime-type text/plain $1
-svn propset svn:eol-style native     $1
-
--- svnpropset.sh
+++
@@ -1,3 +0,0 @@
-svn propset svn:mime-type text/plain $1
-svn propset svn:eol-style native     $1
-
