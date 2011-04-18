ca3032a8f53e04661d480c8a690c06b3605ce1c9
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-18 15:44:28 -0400

    ENH: Now submitting to new Dashboard.
    http://my.cdash.org/index.php?project=LesionSizingToolkit

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,13 @@
-set(CTEST_PROJECT_NAME "Insight")
-set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+## # The following are required to uses Dart and the Cdash dashboard
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+set(CTEST_PROJECT_NAME "LesionSizingToolkit")
+set(CTEST_NIGHTLY_START_TIME "2:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Insight")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=LesionSizingToolkit")
 set(CTEST_DROP_SITE_CDASH TRUE)
