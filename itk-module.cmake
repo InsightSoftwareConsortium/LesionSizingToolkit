f58a868516c69e520c2ee3ca85ab66b0538ccbe9
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2011-05-25 09:15:08 -0400

    ENH: Clean up and enable LSTK_USE_VTK option.
    Remove Utilities/KWStyle due to redundancy.
    
    LSTK_USE_VTK is required to be ON to run programs in
    Examples and Visualization. This commits modifies cmake
    code accordingly to build LSTK with LSTK_USE_VTK ON.

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,1 +1,1 @@
-itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta TEST_DEPENDS ITK-TestKernel)
+itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta ITK-VTK TEST_DEPENDS ITK-TestKernel)
