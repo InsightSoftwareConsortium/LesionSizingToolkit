c3bee7c56b957b15de8b76f7594be79a3b077431
Luis Ibanez <luis.ibanez@kitware.com>
2011-05-25 14:14:33 -0400

    DOC: Add Doxygen documentation at Module level.

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,1 +1,8 @@
-itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta ITK-VTK TEST_DEPENDS ITK-TestKernel)
+set(DOCUMENTATION "This module contains a collection of classes for performing
+measurement of lesions. Its clinical motivation is the study of cancerous lung
+lesions. However, the functionalities developed here are usable in a wider
+range of applications. The Lesion Sizint Toolkit is build upon ITK and adds to
+it specific classes for computing image features that are then combined into a
+detection and measurement layer.")
+
+itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta ITK-VTK TEST_DEPENDS ITK-TestKernel DESCRIPTION "${DOCUMENTATION}")
